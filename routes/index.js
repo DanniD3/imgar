db12db41373e968e66506a4c3bd2b409d1ba53ab
DanniD3 <daa11@hi.is>
2015-11-16 16:29:50 +0000

    add gulp and dbManager in index route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
+'use strict';
 var express = require('express');
 var router = express.Router();
+var dbManager = require('../lib/dbManager');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
