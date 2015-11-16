8a1ad844e7eb1dadf6be93d5767fadad2a4f80a4
Jianfei Zheng <jiz2@hi.is>
2015-11-16 22:39:17 +0000

    Added sql, .env, and login handler

--- create.sql
+++ create.sql
@@ -8,18 +8,3 @@
   CONSTRAINT users_pkey PRIMARY KEY (id),
   CONSTRAINT users_username_key UNIQUE (username)
 );
-
-CREATE TABLE msg
-(
-  id serial NOT NULL,
-  publisher character varying(32) NOT NULL,
-  title character varying(64) NOT NULL,
-  text character varying NOT NULL,
-  date timestamp with time zone,
-  CONSTRAINT msg_pkey PRIMARY KEY (id),
-  CONSTRAINT users_fkey FOREIGN KEY (publisher)
-      REFERENCES users (username) MATCH FULL
-      ON UPDATE RESTRICT ON DELETE NO ACTION,
-  CONSTRAINT msg_text_not_empty CHECK (text::text <> ''::text),
-  CONSTRAINT msg_title_not_empty CHECK (title::text <> ''::text)
-);
