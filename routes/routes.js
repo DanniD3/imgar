09c24438430350ddf22a6f7e4ca48678bdc6569c
Jianfei Zheng <jiz2@hi.is>
2015-11-21 15:25:14 +0000

    Merged upload to index, rerouted+middleware+styles

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,20 @@
 'use strict';
 
+/////////////////////////////////////
+/*             MODULES             */
+/////////////////////////////////////
+
 var express = require('express');
 var router = express.Router();
 var xss = require('xss');
 var multer  = require('multer');
+
+// var dbManager = require('../lib/dbManager');
+var users = require('../lib/users');
+
+/////////////////////////////////////
+/*        UPLOAD & STORAGE         */
+/////////////////////////////////////
 
 var upload = multer({ 
 	fileFilter: function (req, file, cb) {
@@ -41,62 +52,83 @@ var upload = multer({
 	storage: multer.memoryStorage()
 });
 
-// var dbManager = require('../lib/dbManager');
-var users = require('../lib/users');
 
-/* GET Login. */
-router.get('/', function(req, res, next) {
-	/*jshint unused:false*/
-	res.render('index', {
-		title: 'IMGAR VEF2015',
-		cssSrc: '/stylesheets/index.css'
-	});
-});
+/////////////////////////////////////
+/*             ROUTES              */
+/////////////////////////////////////
+
+/* GET Index. */
+router.get('/', displayIndex);
+
 /* POST Login. */
-router.post('/', function(req, res, next) {
-	/*jshint unused:false*/
+router.post('/login', loginHandler, displayIndex);
 
-	var user = xss(req.body.user);
-	var pwd = xss(req.body.pwd);
-
-	users.auth(user, pwd, function(err) {
-		if (err) {
-			console.log(err);
-		}
-
-		res.render('index', {
-			title: 'IMGAR VEF2015',
-			cssSrc: '/stylesheets/index.css',
-			user: user,
-			pwd: pwd,
-			err: err
-		});
-	});
-});
-
-/* GET Upload */
-router.get('/upload', function(req, res, next) {
-	/*jshint unused:false*/
-	res.render('upload', {
-		cssSrc: '/stylesheets/upload.css'
-	});
-});
 /* POST Upload */
 router.post('/upload', upload.single('img'), 
-	function(req, res, next) {
+	uploadHandler, displayIndex);
+
+/////////////////////////////////////
+/*           MIDDLEWARES           */
+/////////////////////////////////////
+
+function displayIndex(req, res, next) {
 	/*jshint unused:false*/
-	/*jshint forin: false */
+	
+	// Initialize user variables
+	console.log(req.session);
+	var vars = {};
+	if (req.session.user) {
+		vars.user = req.session.user;
+	}
+	if (req.session.img) {
+		// var img = req.session.img;
+		// vars.img = img.path;
+		// vars.buffer = img.buffer.toString('base64');
+		vars.buffer = req.session.img;
+	}
+	if (req.session.err) {
+		vars.err = req.session.err;
+	}
+	console.log(vars);
+	// Display
+	res.render('index', {
+		title: 'IMGAR VEF2015',
+		cssSrc: '/stylesheets/index.css',
+		vars: vars
+	});
+}
+
+function loginHandler(req, res, next) {
+	/*jshint unused:false*/
+
+	var username = xss(req.body.user);
+	var pwd = xss(req.body.pwd);
+
+	users.auth(username, pwd, function(err, user) {
+		if (err) {
+			console.log(err);
+			req.session.err = err;
+		}
+
+		if (user) {
+			req.session.regenerate(function (){
+				req.session.user = user;
+			});
+		}
+		req.session.user = username;
+		res.redirect('/');
+	});
+}
+
+function uploadHandler(req, res, next) {
+	/*jshint unused:false*/
 
 	var img = req.file;
+	var imgData = img.buffer.toString('base64');
 
 	console.log(img);
-	console.log(img.buffer.length);
-
-	res.render('upload', {
-		cssSrc: '/stylesheets/upload.css',
-		img: img.path,
-		buffer: img.buffer.toString('base64')
-	});
-});
+	req.session.img = imgData;
+	res.redirect('/');
+}
 
 module.exports = router;
