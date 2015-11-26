b2bb9f31e115fe881ce4c12f51cd699213ee4a89
Jianfei Zheng <jiz2@hi.is>
2015-11-26 22:36:14 +0000

    Added prevention of empty file uploading

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', function() {
+	
+	// Login/Sign Up button events
+	// ---------------------------
 	var items = document.querySelector('.items');
 	var account = document.querySelector('.account');
 	items.addEventListener('click', function () {
@@ -15,4 +18,18 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		}
 	})
+
+	// Blocks Upload when no file present
+	// ----------------------------------
+	var upload = document.querySelector('.upload');
+	var imgInput = document.querySelector('.img');
+	upload.addEventListener('click', function (e) {
+		if (imgInput.files.length === 0) {
+			e.preventDefault();
+			var p = document.createElement('p');
+			p.innerHTML = 'No File chosen for Upload!'
+			p.style.color = 'red';
+			document.querySelector('.err').appendChild(p);
+		}
+	});
 });
