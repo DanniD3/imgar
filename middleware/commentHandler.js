7ca3038ed8b8f0d4747f87bdb74514f5a34c6e13
Jianfei Zheng <jiz2@hi.is>
2015-11-28 14:36:32 +0000

    Messing around with comment form

--- middleware/commentHandler.js
+++ middleware/commentHandler.js
@@ -17,12 +17,5 @@ module.exports = function (req, res, next) {
 	var user = req.session.user.username;
 	var text = xss(req.body.comment);
 
-	var stuff = [];
-	for (var i in req) {
-		stuff.push(i);
-	}
-	console.log(stuff);
-	console.log(req.url);
-	console.log(req.baseUrl, 'base');
-	console.log(req.originalUrl, 'org');
+	res.redirect(xss(req.body.lastUrl));
 }
