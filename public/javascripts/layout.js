00fcb33d1f2e3d95a635039e4d1309a7818a5adc
Jianfei Zheng <jiz2@hi.is>
2015-11-28 14:18:25 +0000

    Now supports dynamic redirect to self for all form

--- public/javascripts/layout.js
+++ public/javascripts/layout.js
@@ -15,18 +15,31 @@ document.addEventListener('DOMContentLoaded', function() {
 	var items = document.querySelector('.items');
 	var account = document.querySelector('.account');
 	var wrapper = document.querySelector('.wrapper');
-	items.addEventListener('click', function () {
-		account.style.display = 'flex';
-		wrapper.style.zIndex = '-1';
-	});
+	if (account) {
+		items.addEventListener('click', function () {
+			account.style.display = 'flex';
+			wrapper.style.zIndex = '-1';
+		});
 
-	window.addEventListener('click', function (e) {
-		if (account.style.display === 'flex') {
-			if (e.srcElement.innerHTML
-				.search(/^\<div/g) !== -1) {
-				account.style.display = 'none';
-				wrapper.style.zIndex = '0';
+		window.addEventListener('click', function (e) {
+			if (account.style.display === 'flex') {
+				if (e.srcElement.innerHTML
+					.search(/^\<div/g) !== -1) {
+					account.style.display = 'none';
+					wrapper.style.zIndex = '0';
+				}
 			}
-		}
-	});
+		});
+	}
+
+	// Add Redirect Url to all Forms
+	// -----------------------------
+	var forms = document.querySelectorAll('form');
+	for (var i = 0; i < forms.length; i++) {
+		var label = document.createElement('label');
+		label.innerHTML = 
+			'<input name=\'lastUrl\' value=\'' +
+			window.location.pathname + '\' hidden/>';
+		forms[i].appendChild(label);
+	}
 });
